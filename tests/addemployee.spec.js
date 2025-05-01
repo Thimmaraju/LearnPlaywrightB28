3744deee7b0bc746b3c8edc8f85d6ce69784d932
G Thimmaraju <thimmaraju418@gmail.com>
2025-05-01 09:01:37 +0530

    data driven testing

--- tests/addemployee.spec.js
+++ tests/addemployee.spec.js
@@ -2,6 +2,12 @@ import { test, expect } from '@playwright/test';
 
 import logindata from "../testdata/login.json"
 
+const employeedetails = {
+
+    firstname : "Raju",
+    lastname : "G"
+}
+
 test('test', async ({ page }) => {
   await page.goto('/web/index.php/auth/login');
   await page.getByRole('textbox', { name: 'Username' }).click();
@@ -12,9 +18,12 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'PIM' }).click();
   await page.getByRole('link', { name: 'Add Employee' }).click();
   await page.getByRole('textbox', { name: 'First Name' }).click();
-  await page.getByRole('textbox', { name: 'First Name' }).fill('Raju');
+  await page.getByRole('textbox', { name: 'First Name' }).fill(employeedetails.firstname);
   await page.getByRole('textbox', { name: 'Last Name' }).click();
-  await page.getByRole('textbox', { name: 'Last Name' }).fill('G');
+  await page.getByRole('textbox', { name: 'Last Name' }).fill(employeedetails.lastname);
   await page.getByRole('button', { name: 'Save' }).click();
   await expect(page.getByRole('heading', { name: 'Personal Details' })).toBeVisible();
+
 });
+
+//Datadriven testing 
